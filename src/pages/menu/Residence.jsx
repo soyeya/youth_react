1cf1e16be944d245ab8e4258e4f91760bb88efdf
박소예 <soye@bagsoyeui-MacBookAir.local>
2024-03-30 14:23:02 +0900

    import axios

--- src/pages/menu/Residence.jsx
+++ src/pages/menu/Residence.jsx
@@ -1,4 +1,5 @@
 import React , { useState, useEffect } from 'react';
+import axios from 'axios';
 import Page from "../../components/Page"
 import Title from '../../components/Title';
 import Navi from '../../components/Navi';
